1b78fc5312281b2cedbf00d80b74c52b51170cc0
SkilzMastr <amdecapio@gmail.com>
2022-05-30 22:55:26 -0400

    Config

--- config.h
+++ config.h
@@ -27,10 +27,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 4,       1,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 4,       1,            0 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Discord",  NULL,       NULL,       1 << 6,       1,            0 },
 	{ "Spotify",  NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Dolphin",  NULL,       NULL,       1 << 4,       0,            0 },
 };
 
 /* layout(s) */
